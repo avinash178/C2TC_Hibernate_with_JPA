e01e069aca532a68dba650002b69cf2c5c827342
avinash178 <avinashrajppure50@gmail.com>
2022-03-08 19:30:04 +0530

    first commit

---
+++ JPA_STUDENT/src/main/java/com/tns/jpa/client/App.java
@@ -0,0 +1,13 @@
+package com.tns.jpa.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
